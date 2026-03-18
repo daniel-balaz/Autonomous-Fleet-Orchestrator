f6734b7f9aaf122ab17f045616e8ea533ac317f3
Michal-Modrovic <michalmodrovic@seznam.cz>
2026-03-18 22:26:19 +0100

    uprava mainu

--- data_module.py
+++ data_module.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
-@dataclass
-class Data:
+@dataclass(frozen=True)
+class Config:
     ID: str = "ROBOT-DEFAULT"
+
+class Data:
+    cena: int = 100
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import asyncio
+from data_module import Data
+
 async def main():
-    pass
+    data = Data()
+    print(data.cena)
 
 if __name__ == "__main__":
     asyncio.run(main())
-    opice = "n test"
-    
