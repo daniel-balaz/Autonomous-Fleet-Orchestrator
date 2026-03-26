50d98ab6b080c87dd7be80306231056ece432bcd
daniel-balaz <danielbalaz07@gmail.com>
2026-03-26 06:47:22 +0100

    Úpravy funkcí v class Robot

--- app/robots/robots_module.py
+++ app/robots/robots_module.py
@@ -1,5 +1,6 @@
 from data_module import SharedData, Config, RobotState
 import random 
+import statistics
 
 class Robot():
     def __init__(self, shared_data: SharedData, cfg: Config, robotstate: RobotState) -> None:
@@ -9,14 +10,32 @@ class Robot():
         self.robotstate = robotstate
 
     def run(self) -> None:
-        self.perform_task()
+        self.robotstate.current_battery = self.perform_task()
+        self.do_battery_state()
 
     def charging(self) -> None:
         pass
-    def do_battery_state(self) -> float:
-        pass
+    def do_battery_state(self) -> None:
+        # 
+        self.robotstate.last_battery_list.append(self.robotstate.current_battery)
+        
+        if len(self.robotstate.last_battery_list) > 1:
+            battery_diff = self.robotstate.last_battery_list[-2] - self.robotstate.last_battery_list[-1]
+            self.robotstate.last_battery_diff.append(battery_diff)
+            if len(self.robotstate.last_battery_diff) > 1: 
+                step_diff = 0
+                for diff in range(len(self.robotstate.last_battery_diff)):
+                    step_diff += self.robotstate.last_battery_diff[diff - 1] - self.robotstate.last_battery_diff[diff]
+                print(self.robotstate.last_battery_diff[diff - 1], self.robotstate.last_battery_diff[diff], step_diff)
 
-    def perform_task(self, robotstate: RobotState) -> int:
-        battery_loss = robotstate.battery_consume_interval + random.randint(-self.cfg.battery_consume_noice, self.cfg.battery_consume_noice)
-        battery_now = round((robotstate.current_battery - battery_loss) + (self.cfg.battery_consume_noice * robotstate.battery_consume_multiplier)) 
+
+
+            median_of_battery_diff = statistics.median(self.robotstate.last_battery_diff)
+            rounds_left = self.robotstate.current_battery / median_of_battery_diff if median_of_battery_diff != 0 else 0
+            print(f"Rounds Left: {round(rounds_left)}")
+        
+
+    def perform_task(self) -> int:
+        battery_loss = self.robotstate.battery_consume_interval + random.randint(-self.cfg.battery_consume_noice, self.cfg.battery_consume_noice)
+        battery_now = max(round((self.robotstate.current_battery - battery_loss) - (self.cfg.battery_consume_noice * self.robotstate.battery_consume_multiplier)), 0) 
         return battery_now
